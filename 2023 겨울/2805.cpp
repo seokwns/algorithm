6d39498b92a22ebe2f4412be293beb499d0fa867
백정은 <jeungeun1130@gmail.com>
2023-01-17 18:28:44 +0900

    [ 2805 - 사람의 수 ] 풀이 추가
    이분 탐색을 이용하여 길이를 탐색하였습니다.
    
    * 주의할 점
    : 잘린 길이는 int 범위를 넘어설 수 있으므로 long long으로 해야 한다.

---
+++ 2023 겨울/2805.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void get_input(vector<int>& trees, int tree_count) {
+    for (int i = 0; i < tree_count; i++) {
+        int tree_length = 0;
+        cin >> tree_length;
+        trees.push_back(tree_length);
+    }
+}
+
+
+// 잘린 나무의 길이 합은 int 범위를 초과할 수 있으므로 long long 으로 선언해줘야 한다.
+long long get_cutted_length(vector<int>& trees, int target_length) {
+    long long result = 0;
+    int len = trees.size();
+    for (int i = 0; i < len; i++) {
+        if (trees[i] > target_length) {
+            int cutted_length = trees[i] - target_length;
+            result += cutted_length;
+        }
+    }
+    return result;
+}
+
+
+int get_maximum_height(vector<int>& trees, int target_length) {
+    int low = 0;
+    int high = *max_element(trees.begin(), trees.end());
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        long long cutted_length = get_cutted_length(trees, mid);
+        if (cutted_length < target_length) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return (low - 1);
+}
+
+
+int main(void) {
+    int tree_count = 0;
+    int target_length = 0;
+    vector<int> trees;
+    cin >> tree_count >> target_length;
+    get_input(trees, tree_count);
+    int maximum_length = get_maximum_height(trees, target_length);
+    cout << maximum_length << endl;
+    return 0;
+}
