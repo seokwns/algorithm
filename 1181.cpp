d1ed3fb50bf04b87b6c916fd3f20476d46d17b4d
SeokjunMoon <seokjun0915@icloud.com>
2023-02-21 08:12:38 +0900

    1181 - 단어정렬 정렬 함수만 잘 짜면 되는 문제였습니다.

--- 1181.cpp
+++ 1181.cpp
@@ -4,20 +4,32 @@ https://www.acmicpc.net/problem/1181
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
+bool compare(string a, string b) {
+    if (a.length() > b.length()) return false;
+    else if (a.length() < b.length()) return true;
+    return a < b;
+}
+
 int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
     cin >> n;
 
     vector<string> words;
     for (int i = 0; i < n; i++) {
         string input;
-        getline(cin, input);
-        words.push_back(input);
+        cin >> input;
+        if (find(words.begin(), words.end(), input) == words.end()) 
+            words.push_back(input);
     }
 
-    
+    sort(words.begin(), words.end(), compare);
+    for (auto i : words) cout << i << "\n";
     return 0;    
 }
